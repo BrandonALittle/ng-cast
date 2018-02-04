5f94300eaa9456b4ff3a5c8fd59f06ec51eded19
BrandonALittle <brandon.a.little@gmail.com>
2018-02-03 17:02:19 -0800

    completed

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,5 +1,29 @@
 angular.module('video-player')
+  .controller('VideoPlayerCtrl', function() {
 
-.component('videoPlayer', {
-  // TODO
-});
+    this.modifyURL = () => {
+    //return "https://www.youtube.com/embed/OPxeCiy0RdY";
+      return this.video ? `https://www.youtube.com/embed/${this.video.id.videoId}` : '';
+    };
+  })
+  .component('videoPlayer', {
+    controller: 'VideoPlayerCtrl',
+    bindings: {
+      video: '<'
+    },
+
+    template:
+  `
+  <div class="video-player loading" ng-if="!$ctrl.video">Please wait...</div>
+  <div class="video-player" ng-if="$ctrl.video">
+    <div class="embed-responsive embed-responsive-16by9">
+      <iframe class="embed-responsive-item" ng-src="{{$ctrl.modifyURL()}}" allowFullScreen></iframe>
+    </div>
+    <div class="video-player-details">
+      <h3>{{$ctrl.video.snippet.title}}</h3>
+      <div>{{$ctrl.video.snippet.description}}</div>
+    </div>
+  </div>
+
+  `
+  });
