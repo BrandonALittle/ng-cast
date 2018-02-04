775c375563ef0f8b5e279ff0149a97bda9b7adea
BrandonALittle <brandon.a.little@gmail.com>
2018-02-03 17:03:09 -0800

    completed

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,4 +1,21 @@
 angular.module('video-player')
-.component('videoList', {
-  // TODO
-});
+  .component('videoList', {
+    bindings: {
+      videos: '<',
+      onClick: '<'
+    },
+
+
+    template:
+    `
+    <ul class="video-list">
+      <video-list-entry
+        ng-repeat="video in $ctrl.videos"
+        video="video"
+        on-click="$ctrl.onClick"
+      />
+    </ul>
+    `
+
+  });
+
