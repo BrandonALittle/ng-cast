b274a57e8e46b6882ba8f660353314dacd7ffb0d
BrandonALittle <brandon.a.little@gmail.com>
2018-02-03 17:03:41 -0800

    completed

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,37 @@
 angular.module('video-player')
+  .controller('AppController', function(youTube) {
 
-.component('app', {
+    this.searchService = youTube;
+    this.selectVideo = (video) => {
+      this.currentVideo = video;
+    };
 
-  template: `<div id="app container">
-              <nav class="navbar">
-                <div class="col-md-6 col-md-offset-3">
-                  <search><h5><em>search</em> component goes here</h5></search>
-                </div>
-              </nav>
-              <div class="row">
-                <div class="col-md-7">
-                  <video-player><h5><em>videoPlayer</em> component goes here</h5></video-player>
-                </div>
-                <div class="col-md-5">
-                  <video-list><h5><em>videoList</em> component goes here</h5></video-list>
-                </div>
-              <div>
-            </div>`
-});
+    this.searchResults = (data) => { // take in a video results set
+      this.videos = data;// set videos and currentvideo
+      this.currentVideo = this.videos[0];
+    };
+
+    youTube.search('puppies', this.searchResults);
+  })
+
+  .component('app', {
+    controller: 'AppController',
+    template:
+    `
+    <div id="app container">
+    <nav class="navbar">
+      <div class="col-md-6 col-md-offset-3">
+        <search service="$ctrl.searchService" result="$ctrl.searchResults"/>
+      </div>
+    </nav>
+    <div class="row">
+      <div class="col-md-7">
+        <video-player video="$ctrl.currentVideo"><h5><em>videoPlayer</em> component goes here</h5></video-player>
+      </div>
+      <div class="col-md-5">
+        <video-list videos="$ctrl.videos" on-click="$ctrl.selectVideo"><h5><em>videoList</em> component goes here</h5></video-list>
+      </div>
+    <div>
+  </div>
+  `
+  });
