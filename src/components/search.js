78d603063bf8320d77d5ec7e578f20887208876d
BrandonALittle <brandon.a.little@gmail.com>
2018-02-03 17:03:26 -0800

    completed

--- src/components/search.js
+++ src/components/search.js
@@ -1,5 +1,25 @@
 angular.module('video-player')
+  .controller('SearchController', function() {
+    this.handleClick = () => {
+      this.service.search(this.input, (data) => {
+        this.result(data);
+      });
+    };
+  })
+  .component('search', {
+    controller: 'SearchController',
+    bindings: {
+      service: '<',
+      result: '<'
+    },
+    template:
+  `
+  <div class="search-bar form-inline">
+    <input class="form-control" type="text" ng-model="$ctrl.input"/>
+    <button class="btn" ng-click="$ctrl.handleClick()">
+      <span class="glyphicon glyphicon-search"></span>
+    </button>
+  </div>
 
-.component('search', {
-  // TODO
-});
+  `
+  });
